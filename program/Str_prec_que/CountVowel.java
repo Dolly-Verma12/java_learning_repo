5d58ada8950bd9f071cbca4c4e355185007907ef
Dolly-Verma12 <dollyverma211@gmail.com>
2026-08-07 23:09:49 +0530

    Count vowel and Consonants

--- program/Str_prec_que/CountVowel.java
+++ program/Str_prec_que/CountVowel.java
@@ -17,12 +17,15 @@ class Count{
     static void fun(String N){
         String V="aeiou";
          int count=0;
+         int Add=0;
        for (int i=0;i<N.length();i++){
          char ch=Character.toLowerCase(N.charAt(i));
             if(V.indexOf(ch)!=-1){
-                count++;
-            
+                count++;     
+           }else if(Character.isLetter(ch)){
+            Add++;
            }
        }System.out.println(count);
+       System.out.println(Add);
     }
     }
