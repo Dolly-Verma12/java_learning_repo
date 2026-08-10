57b173ea97e4378a4030907cb1235685961722b1
Dolly-Verma12 <dollyverma211@gmail.com>
2026-08-10 23:34:51 +0530

    Add nonRepeatatingChar file

---
+++ program/Str_prec_que/nonRepeatatingChar.java
@@ -0,0 +1,31 @@
+package Str_prec_que;
+
+import java.util.Scanner;
+public class nonRepeatatingChar {  
+    static void fun(String st){    
+        char ab=' ';       
+      for(int i=0;i<st.length();i++){
+         char ch=st.charAt(i);
+         boolean ct=false;
+          for(int j=0;j<st.length();j++){
+            char ch2=st.charAt(j);
+            if(i!=j&& ch==ch2){
+                ct=true;
+               break;      
+            }  
+          }if(!ct){
+            ab=ch;
+            break;
+          }
+      } 
+        System.out.println(ab);
+      
+    }
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter : ");
+        String st=s.next();
+        fun(st);
+        s.close();
+    }
+}
